344f600c4d3866e10eb8b1d922d1dd4ffc005634
Keks <keks@htmlacademy.ru>
2024-03-24 11:03:33 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/form/open-close-form.js
@@ -0,0 +1,45 @@
+import { openModal, closeModal, isEscapeKey } from '../util.js';
+import { formValidation } from './validation-form.js';
+import { clickControllBigger, clickControllSmaller } from './scale.js';
+import { initializeImageEffects, closeSlider } from './effect-on-image.js';
+
+const imgUploadOverlayElement = document.querySelector('.img-upload__overlay');
+const imgUploadDescription = imgUploadOverlayElement.querySelector('.text__description');
+const imgUploadHashtag = imgUploadOverlayElement.querySelector('.text__hashtags');
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt) && !imgUploadDescription.contains(document.activeElement) && !imgUploadHashtag.contains(document.activeElement)) {
+    evt.preventDefault();
+    closeUploadForm();
+  }
+};
+
+const onBigPictureClose = (evt) => {
+  evt.preventDefault();
+  closeUploadForm();
+};
+
+function openUploadForm () {
+  openModal(imgUploadOverlayElement);
+  formValidation();
+  initializeImageEffects();
+
+  document.addEventListener('keydown', onDocumentKeydown);
+
+  document.querySelector('.img-upload__cancel').addEventListener('click', onBigPictureClose);
+  document.querySelector('.scale__control--bigger').addEventListener('click', clickControllBigger);
+  document.querySelector('.scale__control--smaller').addEventListener('click', clickControllSmaller);
+}
+
+function closeUploadForm () {
+  closeModal(imgUploadOverlayElement);
+  closeSlider();
+
+  document.removeEventListener('keydown', onDocumentKeydown);
+
+  document.querySelector('.img-upload__cancel').removeEventListener('click', onBigPictureClose);
+  document.querySelector('.scale__control--bigger').removeEventListener('click', clickControllBigger);
+  document.querySelector('.scale__control--smaller').removeEventListener('click', clickControllSmaller);
+}
+
+export { openUploadForm, closeUploadForm };
---
+++ 9/js/gallery/gallery.js
@@ -0,0 +1,20 @@
+import { getArrayPhotoProfiles } from '../create-array-photo-profiles.js';
+import { getRenderingThumbnail } from '../rendering-thumbnail.js';
+import { openBigPicture } from './rendering-big-picture.js';
+
+const data = getArrayPhotoProfiles();
+
+getRenderingThumbnail(data);
+
+document.querySelector('.pictures').addEventListener('click', (evt) => {
+  const parent = evt.target.parentNode;
+
+  if (parent?.dataset?.id) {
+    evt.preventDefault();
+    const profile = data.find((item) => item.id.toString() === parent.dataset.id);
+
+    if (profile) {
+      openBigPicture(profile);
+    }
+  }
+});
