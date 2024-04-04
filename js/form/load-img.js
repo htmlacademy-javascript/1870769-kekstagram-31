b937cedcfad6861b0e02edbbf9ae9236de074783
Karen <inokent1994@gmail.com>
2024-04-04 19:00:44 +0300

    сделал подставку выбраного  фото в форму редактирования

---
+++ js/form/load-img.js
@@ -0,0 +1,27 @@
+const fileChooser = document.querySelector('.img-upload__start input[type=file]');
+const preview = document.querySelector('.img-upload__preview img');
+const previewEffects = document.querySelectorAll('.effects__item span');
+
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
+
+const loadImg = () => {
+  fileChooser.addEventListener('change', () => {
+    const file = fileChooser.files[0];
+    const fileName = file.name.toLowerCase();
+
+    const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+    if (matches) {
+      preview.src = URL.createObjectURL(file);
+      previewEffects.forEach((effect) => {
+        effect.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
+      });
+    }
+  });
+};
+
+const closeLoadImg = () => {
+
+};
+
+export { loadImg, closeLoadImg };
